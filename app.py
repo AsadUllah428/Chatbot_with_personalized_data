fc9c9c73d1a798a4d02c7496b25c4bc42d908429
Asad Munir <asadmunir428@gmail.com>
2025-03-06 13:58:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 
-# Create Flask app
 app = Flask(__name__)
 
-# Mock function to simulate bot's response
+#Test Data
 def get_bot_response(user_message):
     if "Cloths" in user_message.lower():
         return "We have a wide variety of Cloths, what type do you want ?."
@@ -12,12 +11,11 @@ def get_bot_response(user_message):
     else:
         return "I'm happy to help with your inquiries! What else would you like to know?"
 
-# Route to display the chat interface
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# API route for handling chat requests
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.form['message']
